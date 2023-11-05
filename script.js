c038a7a01ff4408e93782170597a3563c8bb5aa8
Piotr Karamon <piotrkaramon3@gmail.com>
2023-11-05 01:57:23 +0100

    fix: finishing touches and fixes

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+// Switching between different sites
+function setupClickHandlers(buttonClassName, pageId) {
+    const pageButtons = document.getElementsByClassName(buttonClassName);
+    for (const button of pageButtons) {
+        button.addEventListener("click", (event) => {
+            event.preventDefault();
+            menu(pageId);
+        })
+    }
+}
+
+
+function menu(pageId) {
+    const pages = document.getElementsByClassName("page");
+    for (const page of pages) {
+        page.classList.remove('page-shown')
+    }
+
+    const newPage = document.getElementById(pageId);
+    newPage.classList.add('page-shown');
+    console.log(newPage)
+
+    const navbarMenu = document.getElementById("menu-btn");
+    navbarMenu.checked = false
+
+    document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+}
+
+const navigationButtons = [
+    { pageId: "home-page", btnClassName: "home-page-btn" },
+    { pageId: "contact-page", btnClassName: "contact-page-btn" },
+    { pageId: "tutorial-page", btnClassName: "tutorial-page-btn" },
+    { pageId: "about-page", btnClassName: "about-page-btn" },
+]
+
+for (const { pageId, btnClassName } of navigationButtons) {
+    setupClickHandlers(btnClassName, pageId);
+}
+
+// Handling contact form and its validation
 function openPopup(title, message) {
     document.getElementById('popup-title').innerText = title;
     document.getElementById('popup-message').innerHTML = message;
@@ -7,7 +48,9 @@ function openPopup(title, message) {
 function closePopup() {
     document.getElementById('popup').style.display = 'none';
 }
-document.getElementById('close-button').addEventListener('click', closePopup)
+
+const closeButton = document.getElementById('close-button')
+closeButton.addEventListener('click', closePopup);
 
 const contactForm = document.getElementById('contact-form');
 const inputFields = new Map([
@@ -27,28 +70,27 @@ function contactFormHandler(event) {
     const errors = validateContactData(contactData);
 
     if (errors.size === 0) {
-        successfulSubmition();
+        successfulSubmission();
     } else {
-        unsuccessfulSubmition(errors);
+        unsuccessfulSubmission(errors);
     }
 
     applyFieldStylesBasedOnValidation(errors);
-
 }
 
 contactForm.addEventListener('submit', contactFormHandler);
 
 
-function successfulSubmition() {
+function successfulSubmission() {
     openPopup("Sukces!", "Wiadomość została dostarczona, w najbliższym czasie odezwę się do Ciebie.")
     for (const field of inputFields.values()) {
         field.value = "";
     }
 }
 
-function unsuccessfulSubmition(errors) {
+function unsuccessfulSubmission(errors) {
     const errorListItems = [];
-    for (const [field, error] of errors.entries()) {
+    for (const [_, error] of errors.entries()) {
         errorListItems.push(`<li>${error}</li>`)
     }
     openPopup("Niestety niektóre dane nie są poprawne", `<ul>${errorListItems.join('\n')}</ul>`)
@@ -67,6 +109,7 @@ function applyFieldStylesBasedOnValidation(errors) {
 // source: https://emailregex.com/
 const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const MESSAGE_MAX_LENGTH = 500;
+
 function validateContactData(contactData) {
     const username = contactData.get("username"),
         email = contactData.get("email"),
@@ -92,3 +135,4 @@ function validateContactData(contactData) {
 
     return errors;
 }
+
